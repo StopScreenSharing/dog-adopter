20b3ced52f716fd66a7eaa843e7b813ddbb963d9
aidadieudonne <aidaharing10@gmail.com>
2025-03-17 17:20:40 -0600

    made first components

---
+++ src/AdoptPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AdoptPage() {
+
+}
+
+export default AdoptPage;
---
+++ src/MyDogs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyDogs() {
+
+}
+
+export default MyDogs;
